659032af15e2569ce2358d844d0fb082dfc47603
drorganvidez <davromorg@alum.us.es>
2023-05-16 20:57:55 +0200

    fix: Fix test suite

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 from dotenv import load_dotenv
 from sqlalchemy import text
-import os
-
 from flaskr import create_app, db
 
-load_dotenv()
+import os
 
+load_dotenv()
 app = create_app()
 
 @app.route('/')
@@ -18,7 +16,6 @@ def hello_world():
 def show_env():
     env_vars = {key: value for key, value in os.environ.items()}
     return jsonify(env_vars)
-
 
 @app.route('/test_db')
 def test_db():
