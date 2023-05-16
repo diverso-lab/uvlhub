82281cbf92be20c9b100e340a5350276348ac4a8
drorganvidez <davromorg@alum.us.es>
2023-05-16 19:13:53 +0200

    feat: Implements unit testing

---
+++ tests/units.py
@@ -0,0 +1,40 @@
+import unittest
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from sqlalchemy import text
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+
+db = SQLAlchemy(app)
+
+class FlaskAppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_hello_world(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+
+    def test_show_env(self):
+        response = self.app.get('/env')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, 'application/json')
+
+    def test_test_db(self):
+        response = self.app.get('/test_db')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['message'], 'Connection to the database successful')
+
+if __name__ == '__main__':
+    unittest.main()
