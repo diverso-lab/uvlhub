fd395297a0c42ba32aa3ee1462bad62fab8b4bb3
drorganvidez <davromorg@alum.us.es>
2023-06-09 21:53:16 +0200

    fix: Fix bug in publication type

--- app/zenodo.py
+++ app/zenodo.py
@@ -1,12 +1,15 @@
-import requests
+"""
+This module contains functions to interact with Zenodo and perform operations related to depositions
+"""
+
 import os
-import json
+import requests
 
 from dotenv import load_dotenv
 from flask_login import current_user
 
 import app
-from app.dataset.models import DataSet
+from app.dataset.models import DataSet, FeatureModel
 
 load_dotenv()
 
@@ -14,14 +17,26 @@ ZENODO_API_URL = 'https://sandbox.zenodo.org/api/deposit/depositions'
 ZENODO_ACCESS_TOKEN = os.getenv('ZENODO_ACCESS_TOKEN')
 
 
-def test_zenodo_connection():
+def test_zenodo_connection() -> bool:
+    """
+    Test the connection with Zenodo.
+
+    Returns:
+        bool: True if the connection is successful, False otherwise.
+    """
     headers = {"Content-Type": "application/json"}
     params = {'access_token': ZENODO_ACCESS_TOKEN}
     response = requests.get(ZENODO_API_URL, params=params, headers=headers)
     return response.status_code == 200
 
 
-def get_all_depositions():
+def get_all_depositions() -> dict:
+    """
+    Get all depositions from Zenodo.
+
+    Returns:
+        dict: The response in JSON format with the depositions.
+    """
     headers = {"Content-Type": "application/json"}
     params = {'access_token': ZENODO_ACCESS_TOKEN}
     response = requests.get(ZENODO_API_URL, params=params, headers=headers)
@@ -30,7 +45,16 @@ def get_all_depositions():
     return response.json()
 
 
-def zenodo_create_new_deposition(dataset: DataSet):
+def zenodo_create_new_deposition(dataset: DataSet) -> dict:
+    """
+    Create a new deposition in Zenodo.
+
+    Args:
+        dataset (DataSet): The DataSet object containing the metadata of the deposition.
+
+    Returns:
+        dict: The response in JSON format with the details of the created deposition.
+    """
     metadata = {
         'title': dataset.ds_meta_data.title,
         'upload_type': 'dataset' if dataset.ds_meta_data.publication_type.value == "none" else 'publication',
@@ -59,7 +83,17 @@ def zenodo_create_new_deposition(dataset: DataSet):
     return response.json()
 
 
-def zenodo_upload_file(deposition_id, feature_model):
+def zenodo_upload_file(deposition_id: int, feature_model: FeatureModel) -> dict:
+    """
+    Upload a file to a deposition in Zenodo.
+
+    Args:
+        deposition_id (int): The ID of the deposition in Zenodo.
+        feature_model (FeatureModel): The FeatureModel object representing the feature model.
+
+    Returns:
+        dict: The response in JSON format with the details of the uploaded file.
+    """
     uvl_filename = feature_model.fm_meta_data.uvl_filename
     data = {'name': uvl_filename}
     user_id = current_user.id
@@ -75,7 +109,16 @@ def zenodo_upload_file(deposition_id, feature_model):
     return response.json()
 
 
-def zenodo_publish_deposition(deposition_id):
+def zenodo_publish_deposition(deposition_id: int) -> dict:
+    """
+    Publish a deposition in Zenodo.
+
+    Args:
+        deposition_id (int): The ID of the deposition in Zenodo.
+
+    Returns:
+        dict: The response in JSON format with the details of the published deposition.
+    """
     headers = {"Content-Type": "application/json"}
     publish_url = f'{ZENODO_API_URL}/{deposition_id}/actions/publish'
     params = {'access_token': ZENODO_ACCESS_TOKEN}
@@ -85,7 +128,16 @@ def zenodo_publish_deposition(deposition_id):
     return response.json()
 
 
-def zenodo_get_deposition(deposition_id):
+def zenodo_get_deposition(deposition_id: int) -> dict:
+    """
+    Get a deposition from Zenodo.
+
+    Args:
+        deposition_id (int): The ID of the deposition in Zenodo.
+
+    Returns:
+        dict: The response in JSON format with the details of the deposition.
+    """
     headers = {"Content-Type": "application/json"}
     deposition_url = f'{ZENODO_API_URL}/{deposition_id}'
     params = {'access_token': ZENODO_ACCESS_TOKEN}
@@ -95,5 +147,14 @@ def zenodo_get_deposition(deposition_id):
     return response.json()
 
 
-def zenodo_get_doi(deposition_id):
+def zenodo_get_doi(deposition_id: int) -> str:
+    """
+    Get the DOI of a deposition from Zenodo.
+
+    Args:
+        deposition_id (int): The ID of the deposition in Zenodo.
+
+    Returns:
+        str: The DOI of the deposition.
+    """
     return zenodo_get_deposition(deposition_id).get('doi')
