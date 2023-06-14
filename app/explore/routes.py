b246ed440c6efdf18e453138cb217c1d2ee3bcbd
David Romero <drorganvidez@us.es>
2023-06-14 14:10:38 +0200

    feat: Implement topbar search

--- app/explore/routes.py
+++ app/explore/routes.py
@@ -10,70 +10,68 @@ from .forms import ExploreForm
 from ..dataset.models import DataSet, DSMetaData, Author, FeatureModel, FMMetaData, PublicationType
 
 
-@explore_bp.route('/explore', methods=['GET'])
+@explore_bp.route('/explore', methods=['GET', 'POST'])
 def index():
-    form = ExploreForm()
-    return render_template('explore/index.html', form=form)
+    if request.method == 'GET':
+        query = request.args.get('query', '')
+        form = ExploreForm()
+        return render_template('explore/index.html', form=form, query=query)
+    elif request.method == 'POST':
+        criteria = request.get_json()
+        query = criteria.get('query', '')
+        order = criteria.get('sorting', 'newest')
+        publication_type = criteria.get('publication_type', 'any')
+        tags = criteria.get('tags', [])
 
+        # Normalize and remove unwanted characters
+        normalized_query = unidecode.unidecode(query).lower()
+        cleaned_query = re.sub(r'[,.]', '', normalized_query)
+        query_words = cleaned_query.split()
 
-@explore_bp.route('/explore', methods=['POST'])
-def explore():
-    criteria = request.get_json()
-    query = criteria.get('query', '')
-    order = criteria.get('sorting', 'newest')
-    publication_type = criteria.get('publication_type', 'any')
-    tags = criteria.get('tags', [])
+        filters = []
+        for word in query_words:
+            filters.append(DSMetaData.title.ilike(f'%{word}%'))
+            filters.append(DSMetaData.description.ilike(f'%{word}%'))
+            filters.append(Author.name.ilike(f'%{word}%'))
+            filters.append(Author.affiliation.ilike(f'%{word}%'))
+            filters.append(Author.orcid.ilike(f'%{word}%'))
+            filters.append(FMMetaData.uvl_filename.ilike(f'%{word}%'))
+            filters.append(FMMetaData.title.ilike(f'%{word}%'))
+            filters.append(FMMetaData.description.ilike(f'%{word}%'))
+            filters.append(FMMetaData.publication_type.ilike(f'%{word}%'))
+            filters.append(FMMetaData.publication_doi.ilike(f'%{word}%'))
+            filters.append(FMMetaData.tags.ilike(f'%{word}%'))
+            filters.append(DSMetaData.tags.ilike(f'%{word}%'))
 
-    # Normalize and remove unwanted characters
-    normalized_query = unidecode.unidecode(query).lower()
-    cleaned_query = re.sub(r'[,.]', '', normalized_query)
-    query_words = cleaned_query.split()
+        datasets = DataSet.query \
+            .join(DSMetaData) \
+            .join(Author) \
+            .join(FeatureModel) \
+            .join(FMMetaData)
 
-    filters = []
-    for word in query_words:
-        filters.append(DSMetaData.title.ilike(f'%{word}%'))
-        filters.append(DSMetaData.description.ilike(f'%{word}%'))
-        filters.append(Author.name.ilike(f'%{word}%'))
-        filters.append(Author.affiliation.ilike(f'%{word}%'))
-        filters.append(Author.orcid.ilike(f'%{word}%'))
-        filters.append(FMMetaData.uvl_filename.ilike(f'%{word}%'))
-        filters.append(FMMetaData.title.ilike(f'%{word}%'))
-        filters.append(FMMetaData.description.ilike(f'%{word}%'))
-        filters.append(FMMetaData.publication_type.ilike(f'%{word}%'))
-        filters.append(FMMetaData.publication_doi.ilike(f'%{word}%'))
-        filters.append(FMMetaData.tags.ilike(f'%{word}%'))
-        filters.append(DSMetaData.tags.ilike(f'%{word}%'))
+        if publication_type != 'any':
+            matching_type = None
+            for member in PublicationType:
+                if member.value.lower() == publication_type:
+                    matching_type = member
+                    break
 
-    datasets = DataSet.query \
-        .join(DSMetaData) \
-        .join(Author) \
-        .join(FeatureModel) \
-        .join(FMMetaData)
+            if matching_type is not None:
+                datasets = datasets.filter(DSMetaData.publication_type == matching_type.name)
 
-    if publication_type != 'any':
-        matching_type = None
-        for member in PublicationType:
-            if member.value.lower() == publication_type:
-                matching_type = member
-                break
+        datasets = datasets.filter(or_(*filters))
 
-        if matching_type is not None:
-            datasets = datasets.filter(DSMetaData.publication_type == matching_type.name)
+        if tags:
+            datasets = datasets.filter(DSMetaData.tags.ilike(any_(f'%{tag}%' for tag in tags)))
 
-    datasets = datasets.filter(or_(*filters))
+        # Order by created_at
+        if order == 'oldest':
+            datasets = datasets.order_by(DataSet.created_at.asc())
+        else:
+            datasets = datasets.order_by(DataSet.created_at.desc())
 
-    if tags:
-        datasets = datasets.filter(DSMetaData.tags.ilike(any_(f'%{tag}%' for tag in tags)))
+        datasets = datasets.all()
 
-    # Order by created_at
-    if order == 'oldest':
-        datasets = datasets.order_by(DataSet.created_at.asc())
-    else:
-        datasets = datasets.order_by(DataSet.created_at.desc())
+        dataset_dicts = [dataset.to_dict() for dataset in datasets]
 
-    datasets = datasets.all()
-
-    dataset_dicts = [dataset.to_dict() for dataset in datasets]
-
-    return jsonify(dataset_dicts)
-
+        return jsonify(dataset_dicts)
