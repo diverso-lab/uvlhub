5b76f1b85bbf7ed60841dab51816fda6c4382cad
drorganvidez <davromorg@alum.us.es>
2024-02-18 19:07:51 +0100

    fix: Update dependencies

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -7,6 +7,6 @@ pip install --upgrade pip
 pip freeze | cut -d = -f 1 | xargs -n1 pip install --upgrade
 
 # Update requirements.txt
-pip freeze > requirements.txt
+pip freeze > ../requirements.txt
 
 echo "Update completed!"
