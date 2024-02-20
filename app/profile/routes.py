1456c37466d78f8271799dbcf6e9fb5df95ffd3d
pparramen <pparra981@gmail.com>
2024-02-20 14:19:57 +0100

    feat: add pagination in user datasets table

--- app/profile/routes.py
+++ app/profile/routes.py
@@ -1,11 +1,15 @@
 from flask import request, render_template, flash, redirect, url_for, Blueprint
 from flask_login import login_required, current_user
+from sqlalchemy.orm import joinedload
+from app import db
 
 from app.profile import profile_bp
 from app.profile.forms import UserProfileForm
+from app.dataset.models import DataSet
 
 from .models import UserProfile
 from .. import get_authenticated_user_profile
+from .services import valid_edit_form  
 
 
 @profile_bp.route('/profile/edit', methods=['GET', 'POST'])
@@ -14,21 +18,7 @@ def edit_profile():
     form = UserProfileForm()
 
     if request.method == 'POST' and form.validate_on_submit():
-        orcid = form.orcid.data.strip()
-        name = form.name.data.strip()
-        surname = form.surname.data.strip()
-        affiliation = form.affiliation.data.strip()
-
-        profile = get_authenticated_user_profile()
-        profile.orcid = orcid
-        profile.name = name
-        profile.surname = surname
-        profile.affiliation = affiliation
-        profile.save()
-
-        flash('Saved profile', 'success')
-
-        return render_template('profile/edit.html', form=form)
+        return valid_edit_form(form)
 
     else:
         return render_template('profile/edit.html', form=form)
@@ -37,6 +27,26 @@ def edit_profile():
 @profile_bp.route('/myProfile')
 @login_required
 def my_profile():
-    user_datasets = current_user.data_sets
-    return render_template('profile/myProfile.html', user_profile=current_user.profile, user=current_user, datasets=user_datasets)
+    page = request.args.get('page', 1, type=int)
+    per_page = 5
+  
+    user_datasets_pagination = db.session.query(DataSet) \
+        .filter(DataSet.user_id == current_user.id) \
+        .paginate(page=page, per_page=per_page, error_out=False)
+    
+    total_datasets_count = db.session.query(DataSet) \
+        .filter(DataSet.user_id == current_user.id) \
+        .count()
+
+    print(user_datasets_pagination.items)
+  
+    return render_template(
+        'profile/myProfile.html', 
+        user_profile=current_user.profile, 
+        user=current_user, 
+        datasets=user_datasets_pagination.items, 
+        pagination=user_datasets_pagination,
+        total_datasets=total_datasets_count  
+    )
+
 
