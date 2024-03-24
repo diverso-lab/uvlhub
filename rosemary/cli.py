809fdc60ab1465841960f696f199347f1bab0121
drorganvidez <davromorg@alum.us.es>
2024-03-24 16:19:05 +0100

    fix: Fix bug in Rosemary

--- rosemary/cli.py
+++ rosemary/cli.py
@@ -4,6 +4,7 @@ import click
 from rosemary.commands.update import update
 from rosemary.commands.info import info
 from rosemary.commands.make_module import make_module
+from rosemary.commands.env import env
 
 
 @click.group()
@@ -14,6 +15,7 @@ def cli():
 cli.add_command(update)
 cli.add_command(info)
 cli.add_command(make_module)
+cli.add_command(env)
 
 if __name__ == '__main__':
     cli()
