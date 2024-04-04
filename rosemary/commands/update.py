148c18699a41305f25219d844240e024496f7125
David Romero <davromorg@alum.us.es>
2024-04-04 11:52:34 +0200

    fix: Fix rosemary

--- rosemary/commands/update.py
+++ rosemary/commands/update.py
@@ -12,16 +12,20 @@ def update():
         subprocess.check_call(['pip', 'install', '--upgrade', 'pip'])
 
         # Get the list of installed packages and update them
-        packages = subprocess.check_output(['pip', 'freeze']).decode('utf-8').split('\n')
+        packages = subprocess.check_output(['pip', 'list', '--format=freeze']).decode('utf-8').split('\n')
         for package in packages:
-            package_name = package.split('==')[0]
-            if package_name:  # Check if the package name is not empty
-                subprocess.check_call(['pip', 'install', '--upgrade', package_name])
+            if not package.startswith("-e"):  # Ignore packages installed in editable mode
+                package_name = package.split('==')[0]
+                if package_name:  # Check if the package name is not empty
+                    subprocess.check_call(['pip', 'install', '--upgrade', package_name])
 
-        # Update requirements.txt
+        # Update requirements.txt, excluding editable installations
         requirements_path = '/app/requirements.txt'
         with open(requirements_path, 'w') as f:
-            subprocess.check_call(['pip', 'freeze'], stdout=f)
+            # Use pip freeze but filter out lines starting with -e
+            freeze_output = subprocess.check_output(['pip', 'freeze']).decode('utf-8')
+            filtered_packages = [line for line in freeze_output.split('\n') if not line.startswith("-e")]
+            f.write('\n'.join(filtered_packages))
 
         click.echo(click.style('Update completed!', fg='green'))
     except subprocess.CalledProcessError as e:
