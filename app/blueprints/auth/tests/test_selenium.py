7a8daad4a056004d39d3953479e7f8fa414159ef
drorganvidez <davromorg@alum.us.es>
2024-05-31 00:43:24 +0200

    feat: Finishing Selenium

--- app/blueprints/auth/tests/test_selenium.py
+++ app/blueprints/auth/tests/test_selenium.py
@@ -8,17 +8,19 @@ from core.selenium.common import initialize_driver, close_driver
 
 
 def test_login_and_check_element():
+
     driver = initialize_driver()
+
     try:
         host = get_host_for_selenium_testing()
 
         # Open the login page
         driver.get(f'{host}/login')
 
-        # Wait a little while to make sure the page has loaded completely.
+        # Wait a little while to make sure the page has loaded completely
         time.sleep(4)
 
-        # Find the username and password field and enter the values.
+        # Find the username and password field and enter the values
         email_field = driver.find_element(By.NAME, 'email')
         password_field = driver.find_element(By.NAME, 'password')
 
@@ -28,16 +30,19 @@ def test_login_and_check_element():
         # Send the form
         password_field.send_keys(Keys.RETURN)
 
-        # Wait a little while to ensure that the action has been completed.
+        # Wait a little while to ensure that the action has been completed
         time.sleep(4)
 
         try:
+
             driver.find_element(By.XPATH, "//h1[contains(@class, 'h2 mb-3') and contains(., 'Latest datasets')]")
             print('Test passed!')
+
         except NoSuchElementException:
             raise AssertionError('Test failed!')
 
     finally:
+
         # Close the browser
         close_driver(driver)
 
