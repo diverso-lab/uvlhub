3999a8f4502d70dd719f0aab12ea4e8a1b1edfb6
drorganvidez <davromorg@alum.us.es>
2024-06-03 18:09:08 +0200

    fix: Fix Render

--- docker/entrypoints/production_entrypoint.sh
+++ docker/entrypoints/production_entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-set -e
-
 # Exit immediately if a command exits with a non-zero status
 set -e
 
--- docker/entrypoints/render_entrypoint.sh
+++ docker/entrypoints/render_entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Wait for the database to be ready by running a script
-sh ./scripts/wait-for-db.sh
-
 # Initialize migrations only if the migrations directory doesn't exist
 if [ ! -d "migrations/versions" ]; then
     # Initialize the migration repository
@@ -22,6 +16,6 @@ flask db migrate
 # Apply the new migrations to the database
 flask db upgrade
 
-# Start the application using Gunicorn, binding it to port 5000
+# Start the application using Gunicorn, binding it to port 80
 # Set the logging level to info and the timeout to 3600 seconds
-exec gunicorn --bind 0.0.0.0:5000 app:app --log-level info --timeout 3600
+exec gunicorn --bind 0.0.0.0:80 app:app --log-level info --timeout 3600
