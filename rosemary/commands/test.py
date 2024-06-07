f0c002ce168e529beb01949656798b1c1fc9b319
David Romero <drorganvidez@us.es>
2024-06-07 08:19:11 +0200

    feat: improve rosemary test

--- rosemary/commands/test.py
+++ rosemary/commands/test.py
@@ -5,7 +5,8 @@ import os
 
 @click.command('test', help="Runs pytest on the blueprints directory or a specific module.")
 @click.argument('module_name', required=False)
-def test(module_name):
+@click.option('-k', 'keyword', help="Only run tests that match the given substring expression.")
+def test(module_name, keyword):
     base_path = os.path.join(os.getenv('WORKING_DIR', ''), 'app/blueprints')
     test_path = base_path
 
@@ -18,7 +19,16 @@ def test(module_name):
     else:
         click.echo("Running tests for all modules...")
 
+    pytest_cmd = ['pytest', '-v', '--ignore-glob=*selenium*', test_path]
+
+    if keyword:
+        pytest_cmd.extend(['-k', keyword])
+
     try:
-        subprocess.run(['pytest', '-v', '--ignore-glob=*selenium*', test_path], check=True)
+        subprocess.run(pytest_cmd, check=True)
     except subprocess.CalledProcessError as e:
         click.echo(click.style(f"Error running tests: {e}", fg='red'))
+
+
+if __name__ == '__main__':
+    test()
