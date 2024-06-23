344302c9001e4f37b781f497d64a90cd7c782771
drorganvidez <davromorg@alum.us.es>
2024-06-23 16:44:46 +0200

    feat: Improve webhook

--- app/blueprints/webhook/routes.py
+++ app/blueprints/webhook/routes.py
@@ -1,57 +1,35 @@
-from flask import Flask
+from flask import request, abort
 import os
-import subprocess
-import docker
 from dotenv import load_dotenv
 from app.blueprints.webhook import webhook_bp
-
+from app.blueprints.webhook.services import WebhookService
 load_dotenv()
 
-app = Flask(__name__)
 WEBHOOK_TOKEN = os.getenv('WEBHOOK_TOKEN')
-
-client = docker.from_env()
 
 
 @webhook_bp.route('/webhook/deploy', methods=['GET'])
 def deploy():
-    '''
+
     token = request.headers.get('Authorization')
     if token != f"Bearer {WEBHOOK_TOKEN}":
-        abort(403)
-    '''
+        abort(403, description="Unauthorized")
 
-    try:
-        web_container = client.containers.get('web_app_container')
-        volume_name = next(
-            (mount.get('Name') or mount.get('Source') for mount in web_container.attrs['Mounts']
-                if mount['Destination'] == '/app'), None
-        )
+    service = WebhookService()
 
-        if not volume_name:
-            raise ValueError("No volume or bind mount found mounted on /app")
+    web_container = service.get_web_container()
+    volume_name = service.get_volume_name(web_container)
 
-    except docker.errors.NotFound:
-        return "Web container not found.", 404
-    except Exception as e:
-        return f"An error occurred: {str(e)}", 500
+    # Pull the latest code on the host
+    service.execute_host_command(volume_name, ['alpine/git', 'pull'])
 
-    try:
-        # Pull the latest code (in host)
-        subprocess.run([
-            'docker', 'run', '--rm',
-            '-v', f'{volume_name}:/app',
-            '-v', '/var/run/docker.sock:/var/run/docker.sock',
-            '-w', '/app',
-            'alpine/git', 'pull'
-        ], check=True)
+    # Update dependencies in the container
+    service.execute_container_command(web_container, 'pip install -r requirements.txt')
 
-        # Update dependencies
-        web_container.exec_run('pip install -r requirements.txt', workdir='/app')
+    # Run migrations in the container
+    service.execute_container_command(web_container, 'flask db upgrade')
 
-        # Run migrations
-        web_container.exec_run('flask db upgrade', workdir='/app')
+    # Log the deployment
+    service.log_deployment(web_container)
 
-        return 'Deployment successful', 200
-    except subprocess.CalledProcessError as e:
-        return f"Deployment failed: {str(e)}", 500
+    return 'Deployment successful', 200
