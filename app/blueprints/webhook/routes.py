edd8d7bee445e7f40ba3bcdfdb745f7b70c70c04
David Romero <drorganvidez@us.es>
2024-06-25 08:15:29 +0200

    fix: Fix webhook

--- app/blueprints/webhook/routes.py
+++ app/blueprints/webhook/routes.py
@@ -8,21 +8,23 @@ load_dotenv()
 WEBHOOK_TOKEN = os.getenv('WEBHOOK_TOKEN')
 
 
-@webhook_bp.route('/webhook/deploy', methods=['POST'])
+@webhook_bp.route('/webhook/deploy', methods=['GET'])
 def deploy():
 
+    '''
     token = request.headers.get('Authorization')
     if token != f"Bearer {WEBHOOK_TOKEN}":
         abort(403, description="Unauthorized")
+    '''
 
     service = WebhookService()
 
     web_container = service.get_web_container()
-    volume_name = service.get_volume_name(web_container)
 
-    # Pull the latest code on the host
-    service.execute_host_command(volume_name, ['alpine/git', 'config', '--global', '--add', 'safe.directory', '/app'])
-    service.execute_host_command(volume_name, ['alpine/git', 'pull'])
+    # Pull the latest code in the container
+    service.execute_container_command(web_container, 'git remote -v')
+    service.execute_container_command(web_container, 'git remote set-url origin https://github.com/diverso-lab/uvlhub')
+    service.execute_container_command(web_container, 'git pull origin main')
 
     # Update dependencies in the container
     service.execute_container_command(web_container, 'pip install -r requirements.txt')
