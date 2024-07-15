12c5166bdf4c08571ebf93341a6765c15d66d5a1
David Romero <drorganvidez@us.es>
2024-07-15 12:03:48 +0200

    fix: Fix linter

--- core/blueprints/base_blueprint.py
+++ core/blueprints/base_blueprint.py
@@ -3,8 +3,13 @@ import os
 
 
 class BaseBlueprint(Blueprint):
-    def __init__(self, name, import_name, static_folder=None, static_url_path=None, template_folder=None, url_prefix=None, subdomain=None, url_defaults=None, root_path=None):
-        super().__init__(name, import_name, static_folder=static_folder, static_url_path=static_url_path, template_folder=template_folder, url_prefix=url_prefix, subdomain=subdomain, url_defaults=url_defaults, root_path=root_path)
+    def __init__(self, name, import_name, static_folder=None, static_url_path=None,
+                 template_folder=None, url_prefix=None, subdomain=None,
+                 url_defaults=None, root_path=None):
+        super().__init__(name, import_name, static_folder=static_folder,
+                         static_url_path=static_url_path, template_folder=template_folder,
+                         url_prefix=url_prefix, subdomain=subdomain,
+                         url_defaults=url_defaults, root_path=root_path)
         self.module_path = os.path.join(os.getenv('WORKING_DIR'), 'app', 'modules', name)
         self.add_script_route()
 
