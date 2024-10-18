c72b239f5da6822c5567b505be90b26cbd31b348
drorganvidez <davromorg@alum.us.es>
2024-10-18 21:17:18 +0200

    feat: Improve rosemary webpack command

--- rosemary/commands/webpack_compile.py
+++ rosemary/commands/webpack_compile.py
@@ -2,35 +2,52 @@ import click
 import os
 import subprocess
 
-# Define la ruta donde están los módulos
+# Define the path where the modules are located
 MODULES_DIR = os.path.join(os.getenv('WORKING_DIR', ''), 'app', 'modules')
 
-# Lista de directorios que no queremos iterar
+# List of directories to exclude from iteration
 EXCLUDED_DIRS = {'.pytest_cache', '__pycache__'}
 
 
-@click.command('webpack:compile', help="Iterate over each module's webpack.config.js and compile them.")
-def webpack_compile():
-    # Iterar sobre cada subdirectorio dentro de 'app/modules'
-    for module in os.listdir(MODULES_DIR):
-        module_path = os.path.join(MODULES_DIR, module)
+@click.command('webpack:compile', help="Compile webpack for one or all modules.")
+@click.argument('module_name', required=False)
+def webpack_compile(module_name):
+    # Check if a specific module name was provided
+    if module_name:
 
-        # Verificar si es un directorio y no está en la lista de excluidos
-        if os.path.isdir(module_path) and module not in EXCLUDED_DIRS:
-            webpack_file = os.path.join(module_path, 'assets', 'js', 'webpack.config.js')
+        # Verify if the module exists
+        module_path = os.path.join(MODULES_DIR, module_name)
+        if not os.path.exists(module_path) or not os.path.isdir(module_path):
+            click.echo(click.style(f"Module '{module_name}' does not exist.", fg='red'))
+            return
 
-            # Verificar si el archivo webpack.config.js existe en este módulo
-            if os.path.exists(webpack_file):
-                click.echo(f"Compiling {module}...")
-                
-                # Construir el comando de Webpack
-                webpack_command = f'npx webpack --config {webpack_file} --mode development'
-                
-                # Ejecutar el comando
-                try:
-                    subprocess.run(webpack_command, shell=True, check=True)
-                    click.echo(click.style(f"Successfully compiled {module}!", fg='green'))
-                except subprocess.CalledProcessError as e:
-                    click.echo(click.style(f"Error compiling {module}: {e}", fg='red'))
-            else:
-                click.echo(click.style(f"No webpack.config.js found in {module}, skipping...", fg='yellow'))
+        # Compile only the specified module
+        compile_module(module_name)
+    else:
+        # Iterate over each subdirectory in 'app/modules'
+        for module in os.listdir(MODULES_DIR):
+            module_path = os.path.join(MODULES_DIR, module)
+
+            # Check if it's a directory and not in the excluded list
+            if os.path.isdir(module_path) and module not in EXCLUDED_DIRS:
+                compile_module(module)
+
+def compile_module(module):
+    module_path = os.path.join(MODULES_DIR, module)
+    webpack_file = os.path.join(module_path, 'assets', 'js', 'webpack.config.js')
+
+    # Check if the webpack.config.js file exists for this module
+    if os.path.exists(webpack_file):
+        click.echo(f"Compiling {module}...")
+        
+        # Build the Webpack command
+        webpack_command = f'npx webpack --config {webpack_file} --mode development'
+        
+        # Run the command
+        try:
+            subprocess.run(webpack_command, shell=True, check=True)
+            click.echo(click.style(f"Successfully compiled {module}!", fg='green'))
+        except subprocess.CalledProcessError as e:
+            click.echo(click.style(f"Error compiling {module}: {e}", fg='red'))
+    else:
+        click.echo(click.style(f"No webpack.config.js found in {module}, skipping...", fg='yellow'))
