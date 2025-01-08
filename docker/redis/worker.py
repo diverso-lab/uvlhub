2478709ae07d85ce4aee7c5fb3e9f7a07ba2d801
drorganvidez <davromorg@alum.us.es>
2025-01-08 19:09:15 +0100

    fix: Fix bug when editing dataset

--- docker/redis/worker.py
+++ docker/redis/worker.py
@@ -1,22 +1,29 @@
 import logging
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
 from dotenv import load_dotenv
 from app import create_app
 
+# Load environment variables
 load_dotenv()
+
+# Configure logger
 logger = logging.getLogger(__name__)
 
-
-if __name__ == '__main__':
-
+if __name__ == "__main__":
+    # Create Flask application and load configuration
     app = create_app()
 
     with app.app_context():
+        # Create Flask application and load configuration
+        redis_connection = app.config["SESSION_REDIS"]
 
-        # Connect to Redis and start listening to the queue
-        listen = ['default']
-        conn = app.config['SESSION_REDIS']
+        # Define the queues to be listened to
+        listen = ["default"]
 
-        with Connection(conn):
-            worker = Worker(list(map(Queue, listen)))
-            worker.work()
+        # Create queue list
+        queues = [Queue(name, connection=redis_connection) for name in listen]
+
+        # Initialise the worker
+        logger.info(f"Starting worker for queues: {listen}")
+        worker = Worker(queues)
+        worker.work()
