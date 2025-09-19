dc2713b04710339505f9bd58f1643ab58dccba5e
David Romero <drorganvidez@us.es>
2025-09-19 18:42:52 +0200

    fix: Deletes ORCID field in ProfileForm

--- app/modules/profile/forms.py
+++ app/modules/profile/forms.py
@@ -1,18 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Optional, Regexp
+from wtforms.validators import DataRequired, Length, Optional
 
 
 class UserProfileForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired(), Length(max=100)])
-    surname = StringField("Surname", validators=[DataRequired(), Length(max=100)])
-    orcid = StringField(
-        "ORCID",
+    surname = StringField(
+        "Surname",
         validators=[
-            Optional(),
-            Length(min=19, max=19, message="ORCID must have 16 numbers separated by dashes"),
-            Regexp(r"^\d{4}-\d{4}-\d{4}-\d{4}$", message="Invalid ORCID format"),
+            DataRequired(),
+            Length(max=100),
         ],
     )
-    affiliation = StringField("Affiliation", validators=[Optional(), Length(min=5, max=100)])
+
+    affiliation = StringField(
+        "Affiliation",
+        validators=[
+            Optional(),
+            Length(min=5, max=100),
+        ],
+    )
     submit = SubmitField("Save profile")
