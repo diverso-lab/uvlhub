ec74501362894f2c5764c1a3699d6eff0aa82844
drorganvidez <davromorg@alum.us.es>
2025-09-28 13:26:14 +0200

    feat: Add Zenodo URL in dataset. Closes #72

--- app/modules/featuremodel/models.py
+++ app/modules/featuremodel/models.py
@@ -5,7 +5,7 @@ class FeatureModel(db.Model):
     __tablename__ = "feature_model"
 
     id = db.Column(db.Integer, primary_key=True)
-    dataset_id = db.Column(db.Integer, db.ForeignKey("datasets.id"), nullable=False)
+    dataset_id = db.Column(db.Integer, db.ForeignKey("datasets.id"), nullable=False, index=True)
 
     # Relaciones
     hubfiles = db.relationship("Hubfile", back_populates="feature_model", lazy=True, cascade="all, delete")
