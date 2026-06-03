7900f86fa2a4cc11952eb808fa4ea49633a5e845
Antonio Jiménez Ortega <antjimort@alum.us.es>
2026-06-03 11:47:02 +0200

    fix(generator): configure static assets for deployment

--- app/modules/generator/__init__.py
+++ app/modules/generator/__init__.py
@@ -1,3 +1,9 @@
 from core.blueprints.base_blueprint import BaseBlueprint
 
-generator_bp = BaseBlueprint("generator", __name__, template_folder="templates")
+generator_bp = BaseBlueprint(
+    "generator",
+    __name__,
+    template_folder="templates",
+    static_folder="assets",
+    static_url_path="/generator",
+)
